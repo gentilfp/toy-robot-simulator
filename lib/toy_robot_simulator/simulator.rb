4844dd00f64240ba371313b21271b07c90ea5b40
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-13 16:07:57 -0300

    Create Simulator with a finite loop that receives commands

--- lib/toy_robot_simulator/simulator.rb
+++ lib/toy_robot_simulator/simulator.rb
@@ -1,4 +1,25 @@
 module ToyRobotSimulator
   class Simulator
+    attr_accessor :controller
+
+    def initialize
+      welcome_message
+      while line = $stdin.gets do
+        break if line.downcase.include? "quit"
+        puts line
+      end
+    end
+
+    def welcome_message
+      puts %Q(
+        This is the Toy Robot Simulator Code Challenge!\n
+        Enter with #{available_commands}\n
+        or type QUIT to end the simulator! Good game!\n
+      )
+    end
+
+    def available_commands
+      ["PLACE", "MOVE", "LEFT", "RIGHT"].join(', ')
+    end
   end
 end
