da17255493fa5ad0bc42c1acc5a6c39658472949
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-13 16:07:33 -0300

    Add first specs for Simulator to accept commands

--- spec/toy_robot_simulator/simulator_spec.rb
+++ spec/toy_robot_simulator/simulator_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe ToyRobotSimulator::Simulator do
+  describe "#initialize" do
+    let(:simulator) { ToyRobotSimulator::Simulator }
+
+    it 'puts welcome message when it starts' do
+      allow($stdin).to receive(:gets).and_return(false)
+      expect_any_instance_of(simulator).to receive(:welcome_message)
+      simulator.new
+    end
+
+    it 'ends when user type QUIT' do
+      allow_any_instance_of(simulator).to receive(:welcome_message)
+      allow($stdin).to receive(:gets).and_return("QUIT")
+      simulator.new
+    end
+  end
 end
