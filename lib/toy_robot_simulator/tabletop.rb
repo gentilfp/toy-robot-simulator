138b7603fc2668ac737fd832c54f6d799dabed26
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-16 08:54:47 -0300

    Implementation of tabletop model, initializing and allowing positions

--- lib/toy_robot_simulator/tabletop.rb
+++ lib/toy_robot_simulator/tabletop.rb
@@ -1,4 +1,30 @@
 module ToyRobotSimulator
   class Tabletop
+    attr_accessor :width, :height
+
+    DEFAULT_WIDTH = 5
+    DEFAULT_HEIGHT = 5
+
+    def initialize(width=nil, height=nil)
+      @width = width || DEFAULT_WIDTH
+      @height = height || DEFAULT_HEIGHT
+    end
+
+    def initialized?
+      !(@width.nil? || @height.nil?)
+    end
+
+    def allowed_position?(x, y)
+      allowed_x?(x) && allowed_y?(y)
+    end
+
+    private
+    def allowed_x? x
+      x >= 0 && x < @width
+    end
+
+    def allowed_y? y
+      y >= 0 && y < @height
+    end
   end
 end
--- spec/toy_robot_simulator/tabletop_spec.rb
+++ spec/toy_robot_simulator/tabletop_spec.rb
@@ -1,4 +1,57 @@
 require 'spec_helper'
 
 describe ToyRobotSimulator::Tabletop do
+  let(:tabletop) { ToyRobotSimulator::Tabletop }
+
+  describe "initialize" do
+    subject { tabletop.new(width, height) }
+
+    context "with default parameters" do
+      let(:width)  { nil }
+      let(:height) { nil }
+
+      it "creates a tabletop 5x5" do
+        expect(subject.width).to eq 5
+        expect(subject.height).to eq 5
+      end
+    end
+
+    context "with custom X,Y parameters" do
+      let(:width)  { 10 }
+      let(:height) { 10 }
+
+      it "creates a X,Y (widthxheight) tabletop" do
+        expect(subject.width).to eq 10
+        expect(subject.height).to eq 10
+      end
+    end
+  end
+
+  describe "initialized?" do
+    subject { tabletop.new }
+
+    context "when initialized" do
+      it "returns true" do
+        expect(subject.initialized?).to eq true
+      end
+    end
+  end
+
+  describe "allowed_position?" do
+    subject { tabletop.new }
+
+    it "returns true when going to a valid position on tabletop" do
+      expect(subject.allowed_position?(2, 3)).to be true
+    end
+
+    context "when going to a not valid position on tabletop" do
+      it "returns false with X valid and Y invalid" do
+        expect(subject.allowed_position?(-1, 3)).to be false
+      end
+
+      it "returns false with X invalid and Y valid" do
+        expect(subject.allowed_position?(2, 10)).to be false
+      end
+    end
+  end
 end
