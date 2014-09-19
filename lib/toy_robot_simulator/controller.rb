2f45d3db4ae854f8ce1661de9c0d6212ef93bf22
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-18 21:25:00 -0300

    Add Controller initialize method with specs

--- lib/toy_robot_simulator/controller.rb
+++ lib/toy_robot_simulator/controller.rb
@@ -2,5 +2,19 @@ module ToyRobotSimulator
   class Controller
 
     attr_accessor :robot, :tabletop
+
+    def initialize(width = nil, height = nil)
+      @robot = Robot.new
+      @tabletop = Tabletop.new(width, height)
+    end
+
+    def place(x, y, facing)
+    end
+
+    def move
+    end
+
+    def turn turn_command
+    end
   end
 end
--- spec/toy_robot_simulator/controller_spec.rb
+++ spec/toy_robot_simulator/controller_spec.rb
@@ -1,4 +1,51 @@
 require 'spec_helper'
 
 describe ToyRobotSimulator::Controller do
+  describe "initialize" do
+    let(:robot)      { ToyRobotSimulator::Robot.new }
+    let(:tabletop)   { ToyRobotSimulator::Tabletop.new(width, height) }
+    let(:controller) { described_class.new(width, height) }
+
+    context "with custom width and height" do
+      let(:width)  { 10 }
+      let(:height) { 10 }
+
+      it "matches robot" do
+        expect(controller.robot.x).to eq robot.x
+        expect(controller.robot.y).to eq robot.y
+        expect(controller.robot.facing).to eq robot.facing
+      end
+
+      it "matches tabletop" do
+        expect(controller.tabletop.width).to eq tabletop.width
+        expect(controller.tabletop.height).to eq tabletop.height
+      end
+    end
+
+    context "with default width and height" do
+      let(:width)  { nil }
+      let(:height) { nil }
+
+      it "matches robot" do
+        expect(controller.robot.x).to eq robot.x
+        expect(controller.robot.y).to eq robot.y
+        expect(controller.robot.facing).to eq robot.facing
+      end
+
+      it "matches tabletop" do
+        expect(controller.tabletop.width).to eq tabletop.width
+        expect(controller.tabletop.height).to eq tabletop.height
+      end
+    end
+  end
+
+  describe "#place" do
+  end
+
+  describe "#move" do
+  end
+
+  describe "#turn" do
+  end
+
 end
