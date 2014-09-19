b8bc406e5e31287a7bb11473418cf823994ef78c
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-19 16:59:53 -0300

    Interpreting commands and sending to Controller in Simulator

--- lib/toy_robot_simulator/simulator.rb
+++ lib/toy_robot_simulator/simulator.rb
@@ -3,10 +3,20 @@ module ToyRobotSimulator
     attr_accessor :controller
 
     def initialize
+      controller = Controller.new
+
       welcome_message
       while line = $stdin.gets do
         break if line.downcase.include? "quit"
-        puts line
+
+        command, arguments = line.split(' ')
+
+        command   = command.downcase.to_sym rescue nil
+        arguments = arguments.split(',') rescue []
+
+        if available_commands.include? command.upcase.to_s
+          controller.send(command, *arguments)
+        end
       end
     end
 
@@ -19,7 +29,7 @@ module ToyRobotSimulator
     end
 
     def available_commands
-      ["PLACE", "MOVE", "LEFT", "RIGHT"].join(', ')
+      ["PLACE", "MOVE", "LEFT", "RIGHT", "REPORT"].join(', ')
     end
   end
 end
