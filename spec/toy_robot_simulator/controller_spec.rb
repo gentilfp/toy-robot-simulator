eaa7057ce345a47873be99637312a783529b6fbf
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-09-19 10:20:57 -0300

    Specs for place and turn in Controller

--- spec/toy_robot_simulator/controller_spec.rb
+++ spec/toy_robot_simulator/controller_spec.rb
@@ -40,12 +40,98 @@ describe ToyRobotSimulator::Controller do
   end
 
   describe "#place" do
+    let(:controller) { (described_class.new) }
+    let!(:place)  { controller.place(x, y, facing) }
+
+    context "when position is out of bound" do
+      let(:x)      { 0 }
+      let(:y)      { 10 }
+      let(:facing) { "NORTH" }
+
+      it "does not place robot" do
+        expect(controller.robot.x).to be_nil
+        expect(controller.robot.y).to be_nil
+        expect(controller.robot.facing).to be_nil
+      end
+    end
+
+    context "when placing is allowed" do
+      let(:x)      { 0 }
+      let(:y)      { 1 }
+      let(:facing) { "NORTH" }
+
+      it "places robot in a specific position" do
+        expect(controller.robot.x).to eq x
+        expect(controller.robot.y).to eq y
+        expect(controller.robot.facing).to eq facing
+      end
+    end
+
+    context "when placing is not allowed" do
+      let(:x)      { "a" }
+      let(:y)      { 1 }
+      let(:facing) { "NORTH" }
+
+      context "and facing is not integer" do
+        it "does not placed robot" do
+          expect(controller.robot.x).to be_nil
+          expect(controller.robot.y).to be_nil
+          expect(controller.robot.facing).to be_nil
+        end
+      end
+
+      context "and facing is not allowed" do
+        let(:x)      { 0 }
+        let(:y)      { 1 }
+        let(:facing) { "ABC" }
+
+        it "does not place robot" do
+          expect(controller.robot.x).to be_nil
+          expect(controller.robot.y).to be_nil
+          expect(controller.robot.facing).to be_nil
+        end
+      end
+    end
   end
 
   describe "#move" do
   end
 
   describe "#turn" do
+    let(:controller) { (described_class.new) }
+    let!(:turn) { controller.turn(command) }
+
+    context "when robot is not placed" do
+      let(:command) { nil }
+
+      it "does not turn robot" do
+        expect(controller.robot.x).to be_nil
+        expect(controller.robot.y).to be_nil
+        expect(controller.robot.facing).to be_nil
+      end
+    end
+
+    context "when robot is placed" do
+      before :each do
+        controller.place(0, 0, "NORTH")
+      end
+
+      context "and turn command is not allowed" do
+        let(:command) { "DIREITA" }
+
+        it "does not turn robot" do
+          expect(controller.robot.facing).to eq "NORTH"
+        end
+      end
+
+      context "and command is valid" do
+        let(:command) { "RIGHT" }
+
+        it "moves robot to specific facing" do
+          expect(controller.robot.facing).to eq "EAST"
+        end
+      end
+    end
   end
 
 end
